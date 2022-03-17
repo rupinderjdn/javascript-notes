589278e4c765d5156994e3d3fe00bebd9ced0cea
rupinderjdn <82199133+rupinderjdn@users.noreply.github.com>
2022-03-17 23:10:42 +0530

    21 dec

--- index6.js
+++ index6.js
@@ -2,8 +2,10 @@ var num1 = document.getElementById('num1')
 var num2 = document.getElementById('num2')
 var operator = document.getElementById('operator')
 var result = document.getElementById('result')
+var button = document.getElementById('btn')
 
 button.addEventListener("click",function(){
+
     if (num1.value === "") {
         alert("Enter the number 1")
     }
@@ -13,17 +15,40 @@ button.addEventListener("click",function(){
     var n1 = Number(num1.value)
     var n2 = Number(num2.value)
     
-    if (operator.value === '+') {
-        result.value = n1 + n2;
-    }
-    else if (operator.value === '-') {
-        result.value = n1 - n2;
-    }
-    else if (operator.value === '*') {
-        result.value = n1 * n2;
-    }
-    else if (operator.value === '/') {
-        result.value = n1 / n2;
+    // if (operator.value === '+') {
+    //     result.value = n1 + n2;
+    // }
+    // else if (operator.value === '-') {
+    //     result.value = n1 - n2;
+    // }
+    // else if (operator.value === '*') {
+    //     result.value = n1 * n2;
+    // }
+    // else if (operator.value === '/') {
+    //     result.value = n1 / n2;
+    // }
+
+    // let's see "eval"
+    // it takes string as an input
+
+    // result.value = eval(num1.value + operator.value + num2.value)
+    // document.getElementById('answer').innerHTML=result.value
+
+    var op = operator.value
+    switch (op) {
+        case '+':
+            result.value = n1 + n2
+            break;
+        case '-':
+            result.value = n1 - n2
+            break;
+        case '*':
+            result.value = n1 * n2
+            break;
+        case '/':
+            result.value = n1 / n2
+            break;
+
     }
 })
 // function Calculate() {
